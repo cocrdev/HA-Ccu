9d59485852bff021cc725e024a938e22158a746d
cocrdev <a.sarbassov@cocrealty.kz>
2023-08-14 16:54:15 +0600

    manual alarm panel init

--- custom_components/ccu/__init__.py
+++ custom_components/ccu/__init__.py
@@ -1,6 +1,9 @@
+# __init__.py
+
 from homeassistant import core
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Ccu component."""
-    hass.helpers.discovery.load_platform('sensor', 'ccu', {}, config)
+    # hass.helpers.discovery.load_platform('sensor', 'ccu', {}, config)
+    hass.helpers.discovery.load_platform('alarm_control_panel', 'ccu', {}, config)
     return True
---
+++ custom_components/ccu/alarm_control_panel.py
@@ -0,0 +1,36 @@
+import aiohttp
+from homeassistant.components.alarm_control_panel import AlarmControlPanelEntity
+import json
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+class CcuAlarmControlPanel(AlarmControlPanelEntity):
+    """Representation of an alarm control panel."""
+
+    def __init__(self):
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the alarm control panel."""
+        return 'My Alarm Control Panel'
+        
+    @property
+    def state(self):
+        """Return the state of the alarm control panel."""
+        return self._state
+
+    async def async_update(self):
+        url = "http://192.168.25.103/state/get/1"  # replace {id} with actual id
+        
+        timeout = aiohttp.ClientTimeout(total=10)  # defining timeout to 10 seconds
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            json_data = await fetch(session, url)
+            json_object = json.loads(json_data)
+            self._state = json_object["Partitions"][0]
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the alarm control panel platform."""
+    async_add_entities([CcuAlarmControlPanel()])

9f76995cb29decda427ff017e8189aa7f9fd6243
cocrdev <a.sarbassov@cocrealty.kz>
2023-08-14 15:54:49 +0600

    entity display fix

--- custom_components/ccu/__init__.py
+++ custom_components/ccu/__init__.py
@@ -2,4 +2,5 @@ from homeassistant import core
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Ccu component."""
+    hass.helpers.discovery.load_platform('sensor', 'ccu', {}, config)
     return True

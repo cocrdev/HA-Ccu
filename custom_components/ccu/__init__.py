472751c11da212dbd04ff00b368b7f80302d49c8
cocrdev <a.sarbassov@cocrealty.kz>
2023-08-14 15:41:53 +0600

    Partition state fix

--- custom_components/ccu/__init__.py
+++ custom_components/ccu/__init__.py
@@ -1,4 +1,10 @@
 from homeassistant import core
+from homeassistant.helpers.entity_platform import async_add_entities
+from sensor import CcuSensor
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the sensor platform."""
+    async_add_entities([CcuSensor()])
 
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:

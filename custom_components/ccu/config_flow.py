0140c22fda5155b872c02ef7b85f09f6ad8cf8bc
cocrdev <141922231+cocrdev@users.noreply.github.com>
2023-08-16 15:21:05 +0600

    ip address user input fix

--- custom_components/ccu/config_flow.py
+++ custom_components/ccu/config_flow.py
@@ -9,14 +9,20 @@ class CcuConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=vol.Schema(
-                    {
-                        vol.Required(CONF_HOST): str,
-                    }
-                ),
-            )
+        errors = {}
+        if user_input is not None:
+            # Validate the user input
+            if CONF_HOST in user_input and user_input[CONF_HOST]:
+                return self.async_create_entry(title="CCU", data=user_input)
+            else:
+                errors[CONF_HOST] = "host_required"
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST): str,
+                }
+            ),
+            errors=errors,
+        )
 
-        return self.async_create_entry(title="CCU", data=user_input)

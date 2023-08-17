d295e32d16f9b182e1b0c748f25238f7d5e217de
cocrdev <a.sarbassov@cocrealty.kz>
2023-08-17 16:15:16 +0600

    json fix 11

--- custom_components/ccu/config_flow.py
+++ custom_components/ccu/config_flow.py
@@ -1,7 +1,9 @@
 # config_flow.py
+from typing import Dict
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST
+import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN
 
 class CcuConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -9,18 +11,18 @@ class CcuConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors = {}
+        errors: Dict[str, str] = {}
         if user_input is not None:
             # Validate the user input
             if CONF_HOST in user_input and user_input[CONF_HOST]:
                 return self.async_create_entry(title="CCU", data=user_input)
             else:
-                errors[CONF_HOST] = "host_required"
+                errors[CONF_HOST] = "host required"
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
                 {
-                    vol.Required(CONF_HOST): str,
+                    vol.Required(CONF_HOST): cv.string,
                 }
             ),
             errors=errors,

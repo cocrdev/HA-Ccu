48f490171f48ce88d6b2231bf2f4a5b5f35bf6f3
cocrdev <a.sarbassov@cocrealty.kz>
2023-08-17 17:01:43 +0600

    setting up 1

--- custom_components/ccu/__init__.py
+++ custom_components/ccu/__init__.py
@@ -1,9 +1,10 @@
 # __init__.py
 from homeassistant import core, config_entries
+from .const import DOMAIN
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Ccu component."""
-    hass.helpers.discovery.load_platform('alarm_control_panel', 'ccu', {}, config)
+    hass.data.setdefault(DOMAIN, {})
     return True
 
 async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry):

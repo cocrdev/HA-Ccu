8ce1b893a05a918ace5bbdc78e163d027cdc7dba
cocrdev <a.sarbassov@cocrealty.kz>
2023-08-18 16:13:17 +0600

    update time type fix

--- custom_components/ccu/__init__.py
+++ custom_components/ccu/__init__.py
@@ -6,6 +6,7 @@ from .const import DOMAIN
 from .ccu import Ccu
 from typing import Dict
 import logging
+from datetime import timedelta
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -18,7 +19,7 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
         _LOGGER,
         name=DOMAIN,
         update_method=ccu.update,
-        update_interval=CONF_SCAN_INTERVAL,
+        update_interval=timedelta(seconds=30),
     )
     await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN] : Dict[str, str] = {
